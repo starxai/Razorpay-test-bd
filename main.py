4b0dff8be1ed3205c684201947f037d2d2366cb9
StarXAi Enterprises <147140857+starxai@users.noreply.github.com>
2025-07-15 07:57:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ origins = [
     ]  # Adjust for your React app
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    # allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True, 
     allow_methods=["*"],
     allow_headers=["*"],
